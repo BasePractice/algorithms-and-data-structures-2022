4b18aeb676c6288cadd945051661732b439a2e93
azamat <azamatfar555@gmail.com>
2022-10-14 10:30:10 +0300

    Первая практическая работа с вводом

--- students/21K0712/21K0712-p01/src/main/java/ru/mirea/practice/s21K0712/Main.java
+++ students/21K0712/21K0712-p01/src/main/java/ru/mirea/practice/s21K0712/Main.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public final class Main {
         static Scanner sc = new Scanner(System.in);
         public static void main(String[] args) {
+            System.out.println("Введите количество элементов массива");
             int[] a = new int[sc.nextInt()];
             double sum = 0, min = 9999999, max = 0;
-
+            System.out.println("Введите элементы массива");
             for (int i = 0; i < a.length; i++) {
                 a[i] = sc.nextInt();
                 sum += a[i];
-                System.out.println(sc);
                 System.out.println(a[i]);
             }
-            System.out.println("Summa: " + sum);
-            System.out.println("Sr arifm: " + sum / a.length);
+            System.out.println("Сумма: " + sum);
+            System.out.println("Ср арифм: " + sum / a.length);
             int j = 0;
             while (j < a.length) {
                 if (a[j] >= max) max = a[j];
@@ -23,11 +23,13 @@ public final class Main {
             }
             System.out.println("Max: " + max);
             System.out.println("Min: " + min);
-
+            System.out.println("Гармонический ряд до какого числа");
             int k = sc.nextInt();
             for (double i = 1; i <= k; i++) {
                 System.out.print(1 / i + " ");
             }
+            System.out.println("\nФакториал до введенного ранее числа");
+            System.out.println(fact(k));
         }
 
         public static double fact(int k) {
@@ -35,7 +37,6 @@ public final class Main {
             for (double i = 1; i <= k; i++) {
                 fact *= i;
             }
-            System.out.println("\n" + fact);
             return fact;
         }
     }
