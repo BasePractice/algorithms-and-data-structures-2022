49b40cb4219267beed482102a34fe5467334bc34
azamat <azamatfar555@gmail.com>
2022-10-14 12:36:12 +0300

    Первая практическая работа с вводом и выводом

--- students/21K0712/21K0712-p01/src/main/java/ru/mirea/practice/s21K0712/Main.java
+++ students/21K0712/21K0712-p01/src/main/java/ru/mirea/practice/s21K0712/Main.java
@@ -11,7 +11,10 @@ public final class Main {
             for (int i = 0; i < a.length; i++) {
                 a[i] = sc.nextInt();
                 sum += a[i];
-                System.out.println(a[i]);
+
+            }
+            for (int i = 0; i < a.length; i++) {
+                System.out.println(a[i] + " ");
             }
             System.out.println("Сумма: " + sum);
             System.out.println("Ср арифм: " + sum / a.length);
