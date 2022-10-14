3fb1963bd55f02f8194ff88a57545ea62db33af3
Ilya Lelikov <lelikov-ilya@inbox.ru>
2022-10-14 16:59:31 +0300

    Прк.рб.1,2, обновл.?

--- students/21K654/21K654-p02/src/main/java/ru/mirea/practice/_21K654/_7/BookShelf.java
+++ students/21K654/21K654-p02/src/main/java/ru/mirea/practice/_21K654/_7/BookShelf.java
@@ -1,8 +1,12 @@
 package ru.mirea.practice._21K654._7;
 
 public class BookShelf {
-    private  Book[] arr;
-    private  int num=arr.length;
+    private final Book[] arr;
+    private final int num;
+    public BookShelf(Book[] arr){
+        this.num=arr.length;
+        this.arr=arr;
+    }
     public Book min(){
         int min=arr[0].getYear();
         int j=-1;
@@ -12,5 +16,18 @@ public class BookShelf {
             }
         }
         return arr[j];
+    }
+    public  Book max(){
+        int max=arr[0].getYear();
+        int j=-1;
+        for(int i=0;i<num;i++){
+            if(arr[i].getYear()>=max){
+                j=i;
+            }
+        }
+        return arr[j];
+    }
+    public  void sortBooks(){
+
     }
 }
