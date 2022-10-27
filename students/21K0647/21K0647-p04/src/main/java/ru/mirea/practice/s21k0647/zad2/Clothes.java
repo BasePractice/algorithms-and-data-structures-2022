d1dd88f0ef9d9b1d7f499cbe4fe4f4f573ea8fc8
kodnennsy <lkvlkvlkv2003@yandex.ru>
2022-10-27 23:23:24 +0300

    Четвертая практическая работа

---
+++ students/21K0647/21K0647-p04/src/main/java/ru/mirea/practice/s21k0647/zad2/Clothes.java
@@ -0,0 +1,15 @@
+package ru.mirea.practice.s21k0647.zad2;
+
+public abstract class Clothes implements MenClothing, WomenClothing{
+    Size s;
+    int price;
+    String color;
+    public void dressMen() {
+
+    }
+
+    public void dressWomen() {
+
+    }
+
+}
---
+++ students/21K0647/21K0647-p04/src/main/java/ru/mirea/practice/s21k0647/zad3/Product.java
@@ -0,0 +1,16 @@
+package ru.mirea.practice.s21k0647.zad3;
+
+import java.util.HashMap;
+
+public class Product {
+
+    String name;
+    int price;
+
+    Product() {}
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
