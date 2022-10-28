2a2fcd8945234370aca2912676f6903d564b40af
kodnennsy <lkvlkvlkv2003@yandex.ru>
2022-10-28 21:01:14 +0300

    Четвертая практическая работа

---
+++ students/21K0647/21K0647-p04/src/main/java/ru/mirea/practice/s21k0647/zad3/Product.java
@@ -0,0 +1,14 @@
+package ru.mirea.practice.s21k0647.zad3;
+
+public class Product {
+
+    String name;
+    int price;
+
+    Product() {}
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
