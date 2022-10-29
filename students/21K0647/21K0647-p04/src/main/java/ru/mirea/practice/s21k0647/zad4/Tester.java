74fed5ed4e98076e6382146ccaa2ccc18247d7c7
kodnennsy <lkvlkvlkv2003@yandex.ru>
2022-10-29 11:57:33 +0300

    Четвертая практическая работа (Исправления PMD)

--- students/21K0647/21K0647-p04/src/main/java/ru/mirea/practice/s21k0647/zad4/Tester.java
+++ students/21K0647/21K0647-p04/src/main/java/ru/mirea/practice/s21k0647/zad4/Tester.java
@@ -2,6 +2,7 @@ package ru.mirea.practice.s21k0647.zad4;
 
 public final class Tester {
     private Tester() {}
+
     public static void main(String[] args) {
         Computer computer1 = new Computer(Stamp.HP, 55000,
                 new Memory(1024, 64, 12),
