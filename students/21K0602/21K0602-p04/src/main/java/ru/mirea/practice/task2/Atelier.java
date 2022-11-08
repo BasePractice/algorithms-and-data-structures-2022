88e3a92be00a72eec2c64411b7930fd3174720fc
zonky <Juphy@yandex.ru>
2022-11-08 14:51:31 +0300

    Добавлены 1,2,3,4 практичексие работы (Тесты пройдены)

---
+++ students/21K0602/21K0602-p04/src/main/java/ru/mirea/practice/task2/Atelier.java
@@ -0,0 +1,26 @@
+package ru.mirea.practice.task2;
+
+public class Atelier {
+
+    public Clothes[] clothes;
+
+    public Atelier(Clothes[] clothes) {
+        this.clothes = clothes;
+    }
+
+    public void dressMan() {
+        for (Clothes clothe : clothes) {
+            if (clothe instanceof ManClothing manClothing) {
+                manClothing.dressMen();
+            }
+        }
+    }
+
+    public void dressWomen() {
+        for (Clothes clothe : clothes) {
+            if (clothe instanceof WomenClothing womenClothing) {
+                womenClothing.dressWomen();
+            }
+        }
+    }
+}
