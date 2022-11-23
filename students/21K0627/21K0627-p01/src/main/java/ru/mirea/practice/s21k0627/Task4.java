9f4d1b6e4d5a8c7c24a5749f7bda626aca68d946
telamones <vvtatatavv@gmail.com>
2022-11-23 17:28:33 +0300

    1 практическая работа, исправление ошибок

--- students/21K0627/21K0627-p01/src/main/java/ru/mirea/practice/s21k0627/Task4.java
+++ students/21K0627/21K0627-p01/src/main/java/ru/mirea/practice/s21k0627/Task4.java
@@ -1,10 +1,10 @@
 package ru.mirea.practice.s21k0627;
 
 public class Task4 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("the first 10 terms of the harmonic series:");
-        for (float i=1; i<11; i++) {
-            float num = (1/i);
+        for (float i = 1; i < 11; i++) {
+            float num = (1 / i);
             System.out.printf("%.2f; ", num);
         }
     }
