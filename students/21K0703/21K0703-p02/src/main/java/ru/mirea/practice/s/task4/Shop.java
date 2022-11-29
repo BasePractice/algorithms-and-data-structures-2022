40ad8f9cf388298018d654950a7ca68543088018
wertu11 <101259319+wertu11@users.noreply.github.com>
2022-11-29 16:10:32 +0300

    Добавлена первая и вторая практические работы

---
+++ students/21K0703/21K0703-p02/src/main/java/ru/mirea/practice/s/task4/Shop.java
@@ -0,0 +1,38 @@
+package ru.mirea.practice.s.task4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop {
+    private List<Computer> catalog = new ArrayList<>();
+
+    public void addComputer(Computer computer) {
+        catalog.add(computer);
+    }
+
+    public void deleteComputer(Computer computer) {
+        catalog.remove(computer);
+    }
+
+    public void searchComputer(Computer computer) {
+        if (catalog.isEmpty()) {
+            System.out.println("Каталог пуст");
+        } else {
+            int count = 0;
+            for (Computer comp : catalog) {
+                if (comp.equals(computer)) {
+                    count += 1;
+                    System.out.println(computer.getBrand() + " " + computer.getModel() + " в наличии");
+                }
+            }
+            if (count == 0) {
+                System.out.println("Товар отсутствует");
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return catalog.toString();
+    }
+}
