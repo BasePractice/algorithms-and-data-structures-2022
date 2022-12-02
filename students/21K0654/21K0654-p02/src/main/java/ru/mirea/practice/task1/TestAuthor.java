e8a92d3c33230df8a6e013a22f9433d2bd4e5a54
Ilya Lelikov <lelikov-ilya@inbox.ru>
2022-12-03 02:42:33 +0300

    prct1,2,3,4,4.1

--- students/21K0654/21K0654-p02/src/main/java/ru/mirea/practice/task1/TestAuthor.java
+++ students/21K0654/21K0654-p02/src/main/java/ru/mirea/practice/task1/TestAuthor.java
@@ -2,7 +2,7 @@ package ru.mirea.practice.task1;
 
 public class TestAuthor {
     public static void main(String[] args) {
-        Author author = new Author("zhigga","gog@mail.ru",'m');
+        Author author = new Author("gogga","gog@mail.ru",'m');
         System.out.println(author.getEmail() + "   " + author.getName() + "   " + author.getGender());
         author.setEmail("gig@mail.ru");
         System.out.println(author.getEmail() + '\n' + author.toString());
