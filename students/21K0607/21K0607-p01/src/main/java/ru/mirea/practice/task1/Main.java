848d04bfa22939082ebbc7e37a02d53d07cab7bb
zonky <Juphy@yandex.ru>
2022-12-05 17:32:42 +0300

    Добавлены 1,2,3 практические работы

---
+++ students/21K0607/21K0607-p01/src/main/java/ru/mirea/practice/task1/Main.java
@@ -0,0 +1,11 @@
+package ru.mirea.practice.task1;
+
+public abstract class Main {
+    public static void main(String[] args) {
+        Function function = new Function();
+        function.arraySumFor(10);
+        function.arraySumWhile(10);
+        function.factorial(10);
+        function.harmonicSeries(10);
+    }
+}
