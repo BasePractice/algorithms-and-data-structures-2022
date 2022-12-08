e51d0a2268cec2bb4c48d05b2485ceb38f6a6042
forevermath@yandex.ru <forevermath@yandex.ru>
2022-12-08 19:29:19 +0300

    Первая и Вторая практические

---
+++ students/21K0625/21K0625-p02/src/main/java/ru/mirea/practice/homeworkjava/TestBall.java
@@ -0,0 +1,12 @@
+package ru.mirea.practice.homeworkjava;
+
+public final class TestBall {
+    private TestBall() {}
+
+    public static void main(String[] args) {
+        Ball b1 = new Ball(100.0, 100.0);
+        System.out.println(b1);
+        b1.move(50.0, 55.0);
+        System.out.println(b1);
+    }
+}
