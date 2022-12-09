f0041d068e6021b9c67080be0f85d026c65f6cf1
VerMishelb <57833258+VerMishelb@users.noreply.github.com>
2022-12-09 23:59:23 +0300

    22

---
+++ students/21K0576/21K0576-p22/src/main/java/ru/mirea/practice/task1/Main.java
@@ -0,0 +1,7 @@
+package ru.mirea.practice.task1;
+
+abstract class Main {
+    public static void main(String[] args) {
+        Rpn.calc();
+    }
+}
--- students/21K0576/21K0576-p22/src/main/java/ru/mirea/practice/task1/Rpn.java
+++ students/21K0576/21K0576-p22/src/main/java/ru/mirea/practice/task1/Rpn.java
@@ -1,4 +1,103 @@
 package ru.mirea.practice.task1;
 
-public class Rpn {
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public abstract class Rpn {
+    private static Stack<Double> stack;
+
+    public static void calc() {
+        stack = new Stack<>();
+        try (Scanner in = new Scanner(System.in)) {
+
+            String[] strings = in.nextLine().split("\\s+");
+            for (String str : strings) {
+                if (str.matches("(\\-?\\d+)")) {
+                    stack.push(Double.parseDouble(str));
+                } else if (str.matches("[*,\\-,\\/,+]$")) {
+                    switch (str) {
+                        case "+":
+                            stack.push(add());
+                            break;
+                        case "*":
+                            stack.push(multiply());
+                            break;
+                        case "-":
+                            stack.push(subtract());
+                            break;
+                        case "/":
+                            try {
+                                stack.push(divide());
+                            } catch (IllegalArgumentException e) {
+                                stack.push((double)0);
+                                System.out.println(e);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+        }
+        System.out.println("Ответ: " + stack.pop());
+    }
+
+    private static Double add() {
+        double a = 0;
+        double b = 0;
+        if (!stack.isEmpty()) {
+            a = stack.pop();
+        }
+        if (!stack.isEmpty()) {
+            b = stack.pop();
+        }
+        System.out.println(a + "+" + b);
+        return a + b;
+    }
+
+    private static Double subtract() {
+        double a = 0;
+        double b = 0;
+        if (!stack.isEmpty()) {
+            a = stack.pop();
+        }
+        if (!stack.isEmpty()) {
+            b = stack.pop();
+        }
+        System.out.println(b + "-" + a);
+        return b - a;
+    }
+
+    private static Double multiply() {
+        double a = 0;
+        double b = 0;
+        if (!stack.isEmpty()) {
+            a = stack.pop();
+        }
+        if (!stack.isEmpty()) {
+            b = stack.pop();
+        }
+        System.out.println(a + "*" + b);
+        return a * b;
+    }
+
+    private static Double divide() throws IllegalArgumentException {
+        double a = 0;
+        double b = 0;
+        if (!stack.isEmpty()) {
+            a = stack.pop();
+        }
+        if (!stack.isEmpty()) {
+            b = stack.pop();
+        }
+        System.out.println(b + "/" + a);
+
+        if (a == 0) {
+            throw new IllegalArgumentException("Деление на ноль");
+        } else {
+            return b / a;
+        }
+    }
 }
