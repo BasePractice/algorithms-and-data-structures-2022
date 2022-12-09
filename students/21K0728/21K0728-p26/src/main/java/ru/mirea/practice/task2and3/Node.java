bda729ed483b08a9d780a9ae6592bf886e38f94f
bleedinEyes <lilaksy666@gmail.com>
2022-12-09 23:42:59 +0300

    Практические работы 25 и 26

---
+++ students/21K0728/21K0728-p26/src/main/java/ru/mirea/practice/task2and3/Node.java
@@ -0,0 +1,11 @@
+package ru.mirea.practice.task2and3;
+
+public class Node<K> {
+    public K dt;
+    public Node nxt;
+
+    public Node(K dt) {
+        this.dt = dt;
+        nxt = null;
+    }
+}
