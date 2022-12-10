0a3196f2cfb4516fde4efe3aaa3c7b12ee08a5e4
telamones <vvtatatavv@gmail.com>
2022-12-10 12:26:38 +0300

    18 практическая работа

---
+++ students/21K0627/21K0627-p18/src/main/java/ru/mirea/practice/s21k0627/task1/Test.java
@@ -0,0 +1,8 @@
+package ru.mirea.practice.s21k0627.task1;
+
+public abstract class Test {
+    public static void main(String[] args) {
+        Exception1 exception = new Exception1();
+        exception.exceptionDemo();
+    }
+}
---
+++ students/21K0627/21K0627-p18/src/main/java/ru/mirea/practice/s21k0627/task4/Test.java
@@ -0,0 +1,8 @@
+package ru.mirea.practice.s21k0627.task4;
+
+public abstract class Test {
+    public static void main(String[] args) {
+        Exception4 exception = new Exception4();
+        exception.exceptionDemo();
+    }
+}
